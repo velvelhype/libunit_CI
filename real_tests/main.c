b4d2c33c1f5bb065bb1eecbe13617fe38ca6509c
kamori <67834628+velvelhype@users.noreply.github.com>
2021-05-27 14:45:55 +0900

    Update main.c

--- real_tests/main.c
+++ real_tests/main.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 int	ft_strlen_launcher(void);
 int	ft_atoi_launcher(void);
 
@@ -5,5 +6,6 @@ int	main(void)
 {
 	ft_strlen_launcher();
 	ft_atoi_launcher();
+	systems("leaks a.out");
 	return (0);
 }
